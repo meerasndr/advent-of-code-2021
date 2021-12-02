83e686963aa0fdccbfbb2a086e65d74f9868f2b5
meerasndr <mirage.sundial@gmail.com>
2021-12-02 15:03:04 +0530

    update 1b

--- Golang/Day01/one.go
+++ Golang/Day01/one.go
@@ -20,12 +20,23 @@ func main() {
 		x, _ := strconv.Atoi(scanner.Text())
 		inputarr = append(inputarr, x)
 	}
-	lastindex := len(inputarr) - 2
+	inputlastindex := len(inputarr) - 2
+	fmt.Println("Question 1a:", getIncreaseCount(inputarr, inputlastindex))
+
+	var sumarr []int
+	sumlastindex := len(inputarr) - 3
+	for n := 0; n <= sumlastindex; n++ {
+		sumarr = append(sumarr, inputarr[n]+inputarr[n+1]+inputarr[n+2])
+	}
+	fmt.Println("Question 1b:", getIncreaseCount(sumarr, len(sumarr)-2))
+}
+
+func getIncreaseCount(inputarr []int, lastindex int) int {
 	incCount := 0
 	for n := 0; n <= lastindex; n++ {
 		if inputarr[n] < inputarr[n+1] {
 			incCount++
 		}
 	}
-	fmt.Println(incCount)
+	return incCount
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### advent-of-code-2021
-My quick attempts in Golang. Will try to optimize code if time permits.
+My quick attempts. Will try to optimize code if time permits.
