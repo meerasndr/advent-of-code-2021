3c1e8f7d24a01bea42e2f8d39a5f92751c95350a
meerasndr <mirage.sundial@gmail.com>
2021-12-02 20:32:04 +0530

    add py solution

---
+++ Day01/one.py
@@ -0,0 +1,20 @@
+from os import closerange
+
+
+f = open('input.txt')
+inputlist = f.read().splitlines()
+f.close()
+inccount = 0
+slidingwindowinc = 0
+slidingwindow = []
+for n in range(0, len(inputlist) - 1):
+    if  n < len(inputlist) - 2:
+        slidingwindow.append(int(inputlist[n]) + int(inputlist[n+1]) + int(inputlist[n+2]))
+    if int(inputlist[n]) < int(inputlist[n+1]):
+        inccount += 1
+for x in range(0, len(slidingwindow) - 1):
+    if slidingwindow[x] < slidingwindow[x+1]:
+        slidingwindowinc += 1
+
+print(inccount)
+print(slidingwindowinc)
