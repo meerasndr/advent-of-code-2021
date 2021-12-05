91d6c3e0a4bf17e73b5cbb0a27b22b952d8cbf04
meerasndr <mirage.sundial@gmail.com>
2021-12-05 19:56:35 +0530

    add lifesupportrating function

--- Day03-powerbinary/three.go
+++ Day03-powerbinary/three.go
@@ -19,14 +19,16 @@ func main() {
 	for scanner.Scan() {
 		inputdata = append(inputdata, scanner.Text())
 	}
-
-	fmt.Println(powerconsumption(inputdata))
+	binarycounts := getbinarycounts(inputdata)
+	majority, minority := powerconsumption(binarycounts)
+	gamma, _ := strconv.ParseInt(majority, 2, 64)
+	epsilon, _ := strconv.ParseInt(minority, 2, 64)
+	fmt.Println(gamma * epsilon)
 	fmt.Println(lifesupportrating(inputdata))
 }
 
-func powerconsumption(inputdata []string) int64 {
-	var binarycounts [12]int
-	var g, e string
+func getbinarycounts(inputdata []string) []int {
+	binarycounts := make([]int, 12)
 	for n := 0; n < len(inputdata); n++ {
 		binary_num := inputdata[n]
 		for dig := 0; dig < 12; dig++ {
@@ -37,21 +39,60 @@ func powerconsumption(inputdata []string) int64 {
 			}
 		}
 	}
-	for n := 0; n < len(binarycounts); n++ {
-		if binarycounts[n] < 0 {
-			g += "0"
-			e += "1"
-		} else {
-			g += "1"
-			e += "0"
-		}
-	}
-	gamma, _ := strconv.ParseInt(g, 2, 64)
-	epsilon, _ := strconv.ParseInt(e, 2, 64)
-	return (gamma * epsilon)
+	return binarycounts
 }
 
-func lifesupportrating(inputdata []string) int {
-	
-	return 1
+func powerconsumption(binarycounts []int) (string, string) {
+	var majority, minority string
+	for n := 0; n < len(binarycounts); n++ {
+		if binarycounts[n] < 0 {
+			majority += "0"
+			minority += "1"
+		} else {
+			majority += "1"
+			minority += "0"
+		}
+	}
+	return majority, minority
+}
+
+func lifesupportrating(inputdata []string) int64 {
+	gen := make([]string, len(inputdata))
+	copy(gen, inputdata)
+	scrub := make([]string, len(inputdata))
+	copy(scrub, inputdata)
+	n := 0 // bit number 0 - 11
+	x := 0 // row number 0 to length of data
+	for n < 12 && len(gen) > 1 {
+		binarycount := getbinarycounts(gen)
+		maj, _ := powerconsumption(binarycount)
+		x = 0
+		for _, val := range gen {
+			if maj[n] == val[n] {
+				gen[x] = val
+				x++
+			}
+		}
+		gen = gen[:x]
+		n++
+	}
+	fmt.Println("Gen", gen)
+	n = 0
+	for n < 12 && len(scrub) > 1 {
+		binarycount_s := getbinarycounts(scrub)
+		_, min := powerconsumption(binarycount_s)
+		x = 0 // row value
+		for _, val := range scrub {
+			if min[n] == val[n] {
+				scrub[x] = val
+				x++
+			}
+		}
+		scrub = scrub[:x]
+		n++
+	}
+	fmt.Println("Scrub", scrub)
+	ox, _ := strconv.ParseInt(gen[0], 2, 64)
+	carb, _ := strconv.ParseInt(scrub[0], 2, 64)
+	return ox * carb
 }
