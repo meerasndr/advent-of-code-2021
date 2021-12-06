648719fdc9006bfaa6da018395f5a70db1ff2967
meerasndr <mirage.sundial@gmail.com>
2021-12-06 15:10:30 +0530

    add load inputdata logic

--- Day04-bingo/four.go
+++ Day04-bingo/four.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
-	bingo := loadinputdata()
+	bingo, boards := loadinputdata()
 	fmt.Println(bingo)
+	fmt.Println(boards)
 }
 
-func loadinputdata() []string {
+func loadinputdata() ([]string, [][][]int) {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println("Error opening input file")
@@ -19,10 +20,36 @@ func loadinputdata() []string {
 	}
 	defer file.Close()
 	bingo := make([]string, 1)
+	boards := make([][][]int, 200)
 	scanner := bufio.NewScanner(file)
+	line_num := 1
+	row_index := 0
+	board_index := 0
+	boards[board_index] = make([][]int, 5)
+	/* boards[board_index] => gives a full 5 by 5 board
+	boards[board_index][row_index] => gives a row in a specific board
+	boards[board_index][row_index][number] => gives a specific number
+	*/
+
 	for scanner.Scan() {
-		bingo = append(bingo, scanner.Text())
-		break
+		singleline := scanner.Text()
+		if line_num != 1 && singleline != "" {
+			boards[board_index][row_index] = make([]int, 5)
+			_, err := fmt.Sscanf(singleline, "%d %d %d %d %d", &boards[board_index][row_index][0], &boards[board_index][row_index][1], &boards[board_index][row_index][2], &boards[board_index][row_index][3], &boards[board_index][row_index][4])
+			if err != nil {
+				fmt.Println("Error", err)
+			}
+			row_index += 1
+			if row_index == 5 {
+				board_index += 1
+				row_index = 0
+				boards[board_index] = make([][]int, 5)
+			}
+		} else if line_num == 1 {
+			bingo = append(bingo, scanner.Text())
+			line_num += 1
+		}
 	}
-	return bingo
+	boards = boards[:board_index]
+	return bingo, boards
 }
